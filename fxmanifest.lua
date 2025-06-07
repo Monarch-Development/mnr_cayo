d4f73dca7ccc46c07825dca300d2b829bdca9a2d
IlMelons <melonss2002@gmail.com>
2025-06-07 11:36:25 +0200

    Bump manifest version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 "yes"
 name "mnr_cayo"
 description "Cayo Perico loader script"
 author "IlMelons"
-version "1.0.0"
+version "1.0.1"
 repository "https://github.com/Monarch-Development/mnr_cayo"
 
 this_is_a_map "yes"
