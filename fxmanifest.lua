013ec200e6d78f3d3597611d981ff001be130b6c
IlMelons <melonss2002@gmail.com>
2025-12-17 20:37:36 +0100

    Update version to 2.0.1 | Beach party dj collision fix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 name 'mnr_cayo'
 description 'Cayo Perico Island Loader'
 author 'IlMelons'
-version '2.0.0'
+version '2.0.1'
 repository 'https://github.com/Monarch-Development/mnr_cayo'
 
 this_is_a_map 'yes'
@@ -13,4 +13,5 @@ this_is_a_map 'yes'
 client_scripts {
     'config/*.lua',
     'client/*.lua',
+
 }
